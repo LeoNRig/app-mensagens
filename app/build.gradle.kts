064c24ff9272d2039cf6065e26332f9eddbc9a81
leonardo nascimento <leonardo.rigonini@gmail.com>
2025-01-14 13:13:00 -0300

    PerfilCommit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 dependencies {
 
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.squareup.picasso:picasso:2.8")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
     implementation(platform("com.google.firebase:firebase-bom:32.1.0"))
@@ -50,6 +51,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("com.google.firebase:firebase-storage-ktx:21.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- app/src/main/java/com/leonr/appmensagem/PerfilActivity.kt
+++ app/src/main/java/com/leonr/appmensagem/PerfilActivity.kt
@@ -1,11 +1,214 @@
 package com.leonr.appmensagem
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.leonr.appmensagem.databinding.ActivityPerfilBinding
+import com.leonr.appmensagem.utils.exibirMensagem
+import com.squareup.picasso.Picasso
+
 
 class PerfilActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityPerfilBinding.inflate(layoutInflater)
+    }
+
+    private var temPermissaoCamera = false
+    private var temPermissaoGaleria = false
+
+    private val firebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+    private val storage by lazy {
+        FirebaseStorage.getInstance()
+    }
+    private val firestore by lazy {
+        FirebaseFirestore.getInstance()
+    }
+
+    private val gerenciadorGaleria = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ){uri ->
+        if(uri != null){
+            binding.imagePerfil.setImageURI(uri)
+            uploadImagemStorage(uri)
+        }else{
+            exibirMensagem("Nenhuma imagem selecionada")
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perfil)
+        setContentView(binding.root)
+        inicializarToolbar()
+        solicitarPermissao()
+        inicializarEventosClique()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        recuperarDadosIniciaisUsuarios()
+    }
+
+    private fun recuperarDadosIniciaisUsuarios() {
+        val idUsuario = firebaseAuth.currentUser?.uid
+        if(idUsuario != null){
+
+            firestore
+                .collection("usuarios")
+                .document(idUsuario)
+                .get()
+                .addOnSuccessListener {documentSnapshot->
+
+                    val dadosUsuarios = documentSnapshot.data
+                    if (dadosUsuarios != null){
+                        val nome = dadosUsuarios["nome"] as String
+                        val foto = dadosUsuarios["foto"] as String
+
+                        binding.editNomePerfil.setText(nome)
+                        if(foto.isNotEmpty()){
+                            Picasso.get()
+                                .load(foto)
+                                .into(binding.imagePerfil)
+                        }
+
+                    }
+
+                }
+        }
+
+    }
+
+
+    private fun uploadImagemStorage(uri: Uri) {
+        val idUsuario = firebaseAuth.currentUser?.uid
+        if( idUsuario!= null){
+            storage
+                .getReference("fotos")
+                .child("usuarios")
+                .child("id")
+                .child("perfil.jpg")
+                .putFile(uri)
+                .addOnSuccessListener {task ->
+
+                    exibirMensagem("Sucesso ao enviar a imagem")
+                    task.metadata?.
+                    reference?.
+                    downloadUrl?.
+                    addOnSuccessListener { url ->
+
+                        val dados = mapOf(
+                            "foto" to url.toString()
+                        )
+                        atualizarDadosPerfil(idUsuario, dados)
+
+                    }
+
+                }.addOnFailureListener{
+
+                    exibirMensagem("Erro ao enviar a imagem")
+
+                }
+        }
+
+    }
+
+    private fun atualizarDadosPerfil(idUsuario: String, dados: Map<String, String>) {
+
+        firestore.collection("usuarios")
+            .document(idUsuario)
+            .update(dados)
+            .addOnSuccessListener {
+                exibirMensagem("Atualizado com Sucesso")
+            }
+            .addOnFailureListener{
+                exibirMensagem("Erro ao atualizar perfil usuário")
+            }
+
+    }
+
+
+    private fun inicializarEventosClique() {
+
+        binding.floatingActionButton.setOnClickListener {
+            if (temPermissaoGaleria){
+                gerenciadorGaleria.launch("image/*")
+            }else{
+                exibirMensagem("Não tem permissão")
+                solicitarPermissao()
+            }
+        }
+
+        binding.btnSalvar.setOnClickListener {
+            val nomeUsuario = binding.editNomePerfil.text.toString()
+            if(nomeUsuario.isNotEmpty()){
+
+                val idUsuario = firebaseAuth.currentUser?.uid
+                if(idUsuario != null){
+                    val dados = mapOf(
+                        "nome" to nomeUsuario
+                    )
+                    atualizarDadosPerfil(idUsuario, dados)
+                }
+
+            }else{
+                exibirMensagem("Preencha o nome para atualizar")
+            }
+        }
+    }
+
+    private fun solicitarPermissao() {
+
+        temPermissaoCamera = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+        temPermissaoGaleria = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_MEDIA_IMAGES
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val listaPermissoesNegadas = mutableListOf<String>()
+        if( !temPermissaoCamera )
+            listaPermissoesNegadas.add(Manifest.permission.CAMERA)
+        if( !temPermissaoGaleria )
+            listaPermissoesNegadas.add(Manifest.permission.READ_MEDIA_IMAGES)
+
+        if(listaPermissoesNegadas.isNotEmpty()){
+            val gerenciadorPermissoes = registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()
+            ){permissoes ->
+
+                temPermissaoCamera = permissoes[Manifest.permission.CAMERA]
+                    ?: temPermissaoCamera
+                temPermissaoGaleria = permissoes[Manifest.permission.READ_MEDIA_IMAGES]
+                    ?: temPermissaoGaleria
+
+
+            }
+            gerenciadorPermissoes.launch(listaPermissoesNegadas.toTypedArray())
+        }
+
+
+    }
+
+    private fun inicializarToolbar() {
+        val toolbar = binding.includeToolbarPerfil.tbPrincipal
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            title = "Editar Perfil"
+            setDisplayHomeAsUpEnabled(true)
+        }
     }
 }
